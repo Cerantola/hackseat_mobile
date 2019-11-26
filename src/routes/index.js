489d7e3c3b0a8cb118580e32219ee6f1c4ee9a64
Iago de Souza Lima <iago.souza069@gmail.com>
2019-11-26 16:14:37 -0300

    Tela de login

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,14 @@ import {View, Text} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Register from '../pages/register';
+import Login from '../pages/login';
 
 const AppNavigator = createStackNavigator(
   {
     Register: {
       screen: Register,
     },
+    Login,
   },
   {
     initialRouteName: 'Register',
